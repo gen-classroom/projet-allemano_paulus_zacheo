2270457d9d9f41761769cec435a0f02e930fd43f
Enzo <enzo.allemano@hotmail.com>
2021-03-25 12:13:10 +0100

    Ajout de la commande Build

---
+++ GenProject/src/main/java/GenProject/command/Build.java
@@ -0,0 +1,26 @@
+package GenProject.command;
+
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Class implémentant la sous commande build.
+ * @author Allemano Enzo
+ */
+
+@Command(name = "build", description = "Build a static site")
+public class Build implements Callable<Integer>
+{
+
+    /**
+     * Méthode pour l'appel de la commande build.
+     */
+
+    @Override public Integer call()
+    {
+        System.out.printf("build");
+        return 0;
+    }
+
+}
