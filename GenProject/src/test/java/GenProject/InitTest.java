0488ba3d936067037735a8f217e45034ebddccc3
Yohann <yohann.paulus@heig-vd.ch>
2021-03-25 12:22:55 +0100

    Adding Test file

---
+++ GenProject/src/test/java/GenProject/InitTest.java
@@ -0,0 +1,20 @@
+package GenProject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class InitTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ GenProject/src/test/java/GenProject/VersionTest.java
@@ -0,0 +1,20 @@
+package GenProject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class VersionTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
