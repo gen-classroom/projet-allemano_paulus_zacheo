992bd457cd9721bfbb37de0e633d6c6b2b3220c2
Yohann <yohann.paulus@heig-vd.ch>
2021-04-06 14:46:44 +0200

    Clean command is working

--- GenProject/src/main/java/GenProject/command/Clean.java
+++ GenProject/src/main/java/GenProject/command/Clean.java
@@ -1,14 +1,28 @@
 package GenProject.command;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
+
+import org.apache.commons.io.FileUtils;
 import picocli.CommandLine.Command;
 
 @Command(name = "clean", description = "Clean a static site")
 public class Clean implements Callable<Integer> {
 
-  @Override public Integer call() {
-    System.out.printf("clean");
-    return 1;
+  @Override public Integer call() throws IOException {
+    File targetDirectory = new File(new File(".").getCanonicalPath());
+    File buildDirectory = new File(targetDirectory.getPath() + "/build");
+    if (buildDirectory.exists()) {
+      try {
+        FileUtils.deleteDirectory(buildDirectory);
+      } catch (IOException ioException) {
+        System.err.println("Error while deleting the build directory");
+      }
+    } else {
+      System.err.println("Build directory does not exist");
+    }
+    return 0;
   }
 
 }
