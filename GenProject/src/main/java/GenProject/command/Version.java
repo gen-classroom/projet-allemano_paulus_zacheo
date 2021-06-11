39d5e2e745d6184690b9c7a3913e11789dda7249
AllemanoEn <enzo.allemano@hotmail.com>
2021-06-11 09:43:04 +0200

    Update Version.java

--- GenProject/src/main/java/GenProject/command/Version.java
+++ GenProject/src/main/java/GenProject/command/Version.java
@@ -16,7 +16,7 @@ public class Version implements Callable<Integer> {
     @Override public Integer call() throws Exception {
 
         MavenXpp3Reader reader = new MavenXpp3Reader();
-        Model model = reader.read(new FileReader("pom.xml")); lgtm [java/input-resource-leak]
+        Model model = reader.read(new FileReader("pom.xml"));
         spec.commandLine().getOut().println("v."+model.getVersion());
         return 1;
     }
